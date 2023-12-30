10aefccd46ceb62714620a9778360a49867cd2e7
Nelson Miranda <nmirandaghn@gmail.com>
2023-12-30 01:17:35 -0600

    Rendering the items list

--- 05-travel_list/src/App.js
+++ 05-travel_list/src/App.js
@@ -1,9 +1,15 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
       <Logo />
       <Form />
-      <PackigList />
+      <PackingList />
       <Stats />
     </div>
   );
@@ -19,9 +25,29 @@ function Form() {
     </div>
   );
 }
-function PackigList() {
-  return <div className="list">List</div>;
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={{ textDecoration: item.packed ? "line-through" : "" }}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
