52fede6efefd5cffb93953e42e47f1722cd1104e
Nelson Miranda <nmirandaghn@gmail.com>
2024-01-08 07:14:12 -0600

    Adding a new effect: Changing page

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -356,6 +356,15 @@ function MovieDetails({
     [selectedId]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+
+      document.title = `Movie | ${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
