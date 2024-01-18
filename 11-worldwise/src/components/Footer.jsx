21fbeaf8de901a5084fa6650e055fab752bcea4f
Nelson Miranda <nmirandaghn@gmail.com>
2024-01-17 21:18:58 -0600

    Nested routes and index route

--- 11-worldwise/src/components/Footer.jsx
+++ 11-worldwise/src/components/Footer.jsx
@@ -2,13 +2,11 @@ import styles from "./Sidebar.module.css";
 
 function Footer() {
   return (
-    <div>
-      <footer className={styles.footer}>
-        <p className={styles.copyright}>
-          &copy; Copyright {new Date().getFullYear()} Worldwise Inc.
-        </p>
-      </footer>
-    </div>
+    <footer className={styles.footer}>
+      <p className={styles.copyright}>
+        &copy; Copyright {new Date().getFullYear()} Worldwise Inc.
+      </p>
+    </footer>
   );
 }
 
