1a8f0566d5bddca19c24c233953b1ad7aa137bf3
Nelson Miranda <nmirandaghn@gmail.com>
2024-02-24 12:27:51 -0600

    Styling the cart

--- 16-fast-react-pizza/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
@@ -30,7 +31,7 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton
         to="/menu"
         className="text-sm text-blue-500 hover:text-blue-900 hover:underline"
@@ -38,14 +39,20 @@ function Cart() {
         &larr; Back to menu
       </LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
 
-        <button>Clear cart</button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
