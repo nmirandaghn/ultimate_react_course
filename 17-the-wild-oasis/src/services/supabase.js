2ea9ce0b23e8d32ac7013f68dc2afd66eb0236b1
Nelson Miranda <nmirandaghn@gmail.com>
2024-03-30 20:58:39 -0600

    Connecting Supabase with our React App

---
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://ssxuonrmhsuvhpuxrgaj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzeHVvbnJtaHN1dmhwdXhyZ2FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MjA5NjQsImV4cCI6MjAyNzM5Njk2NH0.fqb6eEE-gHAzCIU1RKKo3KFXt8uG2lLHN-YQftmkl0s";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
