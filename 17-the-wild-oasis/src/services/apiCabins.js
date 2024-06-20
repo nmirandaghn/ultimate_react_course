dcd1efb9bc24a9e1ba5cba49ae45515fa67cff89
Nelson Miranda <nmirandaghn@gmail.com>
2024-06-20 07:01:40 -0600

    Uploading Images to Supabase

--- 17-the-wild-oasis/src/services/apiCabins.js
+++ 17-the-wild-oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -23,14 +23,38 @@ export async function deleteCabin(id) {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1. Create cabin
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be created");
   }
+
+  // 2. Upload image
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  // 3. Delete the cabin IF there was an error in uploading image
+
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    console.log(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabing was not created"
+    );
+  }
+
   return data;
 }
--- 17-the-wild-oasis/src/services/supabase.js
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://ssxuonrmhsuvhpuxrgaj.supabase.co";
+export const supabaseUrl = "https://ssxuonrmhsuvhpuxrgaj.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNzeHVvbnJtaHN1dmhwdXhyZ2FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4MjA5NjQsImV4cCI6MjAyNzM5Njk2NH0.fqb6eEE-gHAzCIU1RKKo3KFXt8uG2lLHN-YQftmkl0s";
 const supabase = createClient(supabaseUrl, supabaseKey);
